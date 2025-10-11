5581c84534086f95afed7a5ac3cbfee7b1421c76
Softline <alehandro.og@bk.ru>
2025-10-12 02:45:06 +0300

    Updated HomePage.tsx

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -110,13 +110,11 @@ const HomePage: React.FC = () => {
           </div>
 
           <div className="flex-1">
-            <div className="relative w-full">
-              <img
-                src="https://i.imgur.com/eHO2mdd.jpeg"
-                alt="SmartChain Analytics Dashboard"
-                className="w-full h-auto object-contain"
-              />
-            </div>
+            <img
+              src="https://i.imgur.com/eHO2mdd.jpeg"
+              alt="SmartChain Analytics Dashboard"
+              className="w-full h-auto"
+            />
           </div>
         </div>
       </section>
